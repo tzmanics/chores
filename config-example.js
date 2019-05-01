99a3df6df8c9df2d62af66eab2063cbdec3d4313
tzmanics <tzmanics@gmail.com>
2019-05-01 09:49:09 -0600

    add Kinvey config

---
+++ config-example.js
@@ -0,0 +1,12 @@
+// this is an example of what your config file would look like
+// your actual config file should be added to your .gitignore file
+// and never see the light of day online :p
+
+const config ={
+  kinvey: {
+    appKey: 'kid_S16j3xVFN',
+    appSecret: 'e0a009c5a6f84949a8310e8c24ff2b7f'
+  }
+}
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,12 @@
+const Kinvey = require('kinvey-node-sdk');
+const config = require('./config-example');
+
+Kinvey.init(config.kinvey);
+
+// optional code to test for successful connection
+Kinvey.ping().then((response) => {
+  console.log(`Kinvey Ping Success! Response: ${response.kinvey}`);
+  }).catch((error) => {
+    console.log(`Kinvey Ping Failed. Resonse: ${error.description}`);
+  });
+
